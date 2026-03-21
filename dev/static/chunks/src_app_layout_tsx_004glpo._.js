02dfb4b59513a2bdfaeb583712fb7b00145693fe
igorczarr <igorczarr@gmail.com>
2026-03-21 18:05:45 -0300

    fix: removendo arquivos de cache e pastas temporárias

--- dev/static/chunks/src_app_layout_tsx_004glpo._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_13-7rdq._.js","static/chunks/src_app_layout_tsx_0ful-2h._.js","static/chunks/src_app_globals_0p2ml0n.css"],
-    source: "dynamic"
-});
